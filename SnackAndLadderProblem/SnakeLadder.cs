4f3af4aac9d3ac00568ba72f636d0bcd8f5f2b2a
Rahulgautama <rahul8874950727@gmail.com>
2023-05-07 11:23:22 +0530

    Snake and Ladder Take Position

--- SnackAndLadderProblem/SnakeLadder.cs
+++ SnackAndLadderProblem/SnakeLadder.cs
@@ -6,14 +6,57 @@ namespace SnackAndLadderProblem
 {
     class SnakeLadder
     {
-        static int position =0;
+        static int position =1;
+        static int die;
         public static void SnakeLadderGame()
         {
-            Console.WriteLine("Position:- "+SnakeLadder.position);
             Random random = new Random();
-            int dice = random.Next(1,6);
-
-            Console.WriteLine("Position:- "+(SnakeLadder.position+dice));
+            Console.WriteLine("Position:- " + SnakeLadder.position);
+            
+            while (true)
+                {
+                die = random.Next(1, 6);
+                if ((position + die) == 11)
+                {
+                    position = position + die + 10;
+                    Console.WriteLine("Take a 11 to 21 Ladder \n21");
+                }                
+                if ((position + die) == 49)
+                {
+                    position = position + die + 32;
+                    Console.WriteLine("Take a 49 to 81 Ladder \n81");
+                }
+                if ((position + die) == 26)
+                {
+                    position = (position + die) - 20;
+                    Console.WriteLine("Take a 26 to 6 snack \n6");
+                }
+                if ((position + die) == 45)
+                {
+                    position = (position + die) - 25;
+                    Console.WriteLine("Take a 45 to 20 snack \n20");
+                }
+                if ((position+die)<100&& (position + die) != 94&& (position + die) != 95 && (position + die) != 96 && (position + die) != 97 && (position + die) != 98 && (position + die) != 99&& (position + die) != 100)
+                {
+                    position += die;
+                    Console.WriteLine(position);
+                }
+                if ((position + die) == 100 || (position + die) == 94 || (position + die) == 95 || (position + die) == 96 || (position + die) == 97 || (position + die) == 98 || (position + die) == 99)
+                {
+                    position += die;
+                    Console.WriteLine(position);
+                }
+                else if (position == 100)
+                    break;
+               
+                
+            }
         }
-    }
+
+   }
+                
 }
+            
+        
+    
+
